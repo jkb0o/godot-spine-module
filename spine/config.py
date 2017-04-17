162681dc50cb435de3fd70beb8df52b9579d6278
Yakov Borevich <j.borevich@gmail.com>
2017-04-17 18:46:58 +0300

    Long int needed for spine

--- spine/config.py
+++ spine/config.py
@@ -5,9 +5,7 @@ def can_build(platform):
   
   
 def configure(env):
-  # was required at some point, looks working witout it for now
-  #env.Append(CPPFLAGS=['-DNEED_LONG_INT'])
-  pass
+  env.Append(CPPFLAGS=['-DNEED_LONG_INT'])
   
   
   
