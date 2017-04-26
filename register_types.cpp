4dca7955552f266217579997783411b51579788f
Yakov Borevich <j.borevich@gmail.com>
2017-04-26 21:14:51 +0300

    Support for binary .skel files

--- register_types.cpp
+++ register_types.cpp
@@ -37,6 +37,7 @@
 #include "spine.h"
 
 #include "core/os/file_access.h"
+#include "core/os/os.h"
 #include "core/io/resource_loader.h"
 #include "scene/resources/texture.h"
 
@@ -58,6 +59,7 @@ void _spAtlasPage_disposeTexture(spAtlasPage* self) {
 	memdelete(ref);
 }
 
+StringArray _sp_invalid_names;
 char* _spUtil_readFile(const char* p_path, int* p_length) {
 
 	FileAccess *f = FileAccess::open(p_path, FileAccess::READ);
@@ -70,10 +72,72 @@ char* _spUtil_readFile(const char* p_path, int* p_length) {
 	data = (char *)_malloc(*p_length, __FILE__, __LINE__);
 	ERR_FAIL_COND_V(data == NULL, NULL);
 	f->get_buffer((uint8_t *)data, *p_length);
-	if (str_path.ends_with(".json") || str_path.ends_with(".atlas")){
+	
+	/*
+	 * A pretty hacky part of patching Spine files
+	 * Preface: If I create spine atlas from images with some deep
+	 *			foldres structure (e.g. head/eyes.png, head/face.png, body/torso.png, ets)
+	 *			I can get a bit buggy ordering in animations.
+	 *			This mostly because of names with slashes in resulting atlas and attachments
+	 *			(head/eyes, head/face, body/torso, etc.)
+	 * I'm to lazy to make any spine research and have no time for waiting this issue
+	 * to be resolved, so I just remember all names with slashes in atlas, replace slashes with 
+	 * '-' and make same thing with skeleton (.json or .skel) for matching names.
+	 * 
+	 * It works good for me, but may be buggy in your case.
+	 * I promice:
+	 * - to create issue about this github
+	 * - add option to avoid this hacky part available in scons (eg hacky_spine=no)
+	 * - may be find actual reason of my buggy animations
+	 *
+	 */
+	
+	if (str_path.ends_with(".atlas")){
+		String current_string = "";
+		bool is_invalid = false;
 		for (int i=0; i<*p_length; i++){
-			if (data[i]=='/') data[i] = '-';
+			if (data[i] == '\n'){
+				if (is_invalid) {
+					_sp_invalid_names.push_back(current_string);
+				}
+				current_string = "";
+				is_invalid = false;
+			} else {
+				current_string += data[i];
+			}
+			if (data[i]=='/') {
+				is_invalid = true;
+				data[i] = '-';
+			}
 		}
+	}
+	if (_sp_invalid_names.size() && (str_path.ends_with(".json") || str_path.ends_with(".skel"))){
+		for (int i=0; i<*p_length; i++){
+			if (data[i] != '/') continue;
+			for (int j=0; j<_sp_invalid_names.size(); j++){
+				String in = _sp_invalid_names[j];
+				int idx = in.find("/");
+				bool match = true;
+				for (int k=0;k<idx;k++){
+					if (i<k) { match=false; break; }
+					if (data[i-k] != in[idx-k]){ match=false; break; }
+				}
+				if (!match) continue;
+				
+				IntArray extra_replaces;
+				for (int k=1; k<in.length()-idx;k++){
+					if (i+k >= *p_length){ match=false; break; }
+					if (data[i+k] != in[idx+k]) { match=false; break; }
+					if (data[i+k] == '/') extra_replaces.push_back(i+k);
+				}
+				if (match){
+					data[i] = '-';
+					for (int k=0; i<extra_replaces.size(); i++) data[k] = '-';
+					break;
+				}
+			}
+		}
+		_sp_invalid_names.resize(0);
 	}
 
 	memdelete(f);
@@ -96,28 +160,40 @@ class ResourceFormatLoaderSpine : public ResourceFormatLoader {
 public:
 
 	virtual RES load(const String &p_path, const String& p_original_path = "", Error *p_err=NULL) {
-
+		float start = OS::get_singleton()->get_ticks_msec();
 		Spine::SpineResource *res = memnew(Spine::SpineResource);
 		Ref<Spine::SpineResource> ref(res);
-
-		String p_atlas = p_path.replace(".json", ".atlas");
+		String p_atlas = p_path.basename() + ".atlas";
 		res->atlas = spAtlas_createFromFile(p_atlas.utf8().get_data(), 0);
 		ERR_FAIL_COND_V(res->atlas == NULL, RES());
-		spSkeletonJson *json = spSkeletonJson_create(res->atlas);
-		ERR_FAIL_COND_V(json == NULL, RES());
-		json->scale = 1;
-
-		res->data = spSkeletonJson_readSkeletonDataFile(json, p_path.utf8().get_data());
-		ERR_EXPLAIN(json->error ? json->error : "");
-		ERR_FAIL_COND_V(res->data == NULL, RES());
-		spSkeletonJson_dispose(json);
-
+		
+		if (p_path.extension() == "json"){
+			spSkeletonJson *json = spSkeletonJson_create(res->atlas);
+			ERR_FAIL_COND_V(json == NULL, RES());
+			json->scale = 1;
+			
+			res->data = spSkeletonJson_readSkeletonDataFile(json, p_path.utf8().get_data());
+			spSkeletonJson_dispose(json);
+			ERR_EXPLAIN(json->error ? json->error : "");
+			ERR_FAIL_COND_V(res->data == NULL, RES());
+		} else {
+			spSkeletonBinary* bin  = spSkeletonBinary_create(res->atlas);
+			ERR_FAIL_COND_V(bin == NULL, RES());
+			bin->scale = 1;
+			res->data = spSkeletonBinary_readSkeletonDataFile(bin, p_path.utf8().get_data());
+			spSkeletonBinary_dispose(bin);
+			ERR_EXPLAIN(bin->error ? bin->error : "");
+			ERR_FAIL_COND_V(res->data == NULL, RES());
+		}
+			
 		res->set_path(p_path);
+		float finish = OS::get_singleton()->get_ticks_msec();
+		print_line("Spine resource (" + p_path + ") loaded in " + itos(finish-start) + " msecs");
 		return ref;
 	}
 
 	virtual void get_recognized_extensions(List<String> *p_extensions) const {
-
+		p_extensions->push_back("skel");
 		p_extensions->push_back("json");
 		p_extensions->push_back("atlas");
 	}
@@ -130,7 +206,7 @@ public:
 	virtual String get_resource_type(const String &p_path) const {
 
 		String el = p_path.extension().to_lower();
-		if (el=="json")
+		if (el=="json" || el=="skel")
 			return "SpineResource";
 		return "";
 	}
